3dfe4b901fbb9011c1eb9b831099c24a8abef16b
Corentin <superlkn35@gmail.com>
2019-08-21 17:54:12 +0200

    Ajout du fichier config

---
+++ site/_config.php
@@ -0,0 +1,20 @@
+<?php
+
+/*** Configuration ***/
+
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+$root = $_SERVER['DOCUMENT_ROOT'];
+$host = $_SERVER['HTTP_HOST'];
+
+define('HOST', 'http://'.$host.'/P4OC/site/');
+define('ROOT', $root.'/P4OC/site/');
+
+define('CONTROLLERFRONT', ROOT.'controller/frontend/');
+define('CONTROLLERBACK', ROOT.'controller/backend/');
+define('VIEWFRONT', ROOT.'view/frontend/');
+define('VIEWBACK', ROOT.'view/backend/');
+define('MODEL', ROOT.'model/');
+
+define('ASSETS', HOST.'assets/css/');
